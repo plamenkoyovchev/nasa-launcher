59b0bde7cb5bf0e004d00e0817ced0e759d442d2
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-28 19:15:55 +0300

    Configure CORS.

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 // middleware
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 
 // routes
