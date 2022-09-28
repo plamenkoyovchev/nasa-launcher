00b95efc6b8cec9f51946a5ce5cd2e43b0a1fcb2
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-28 18:44:32 +0300

    Add planets endpoint initial structure.

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const planetsRouter = require('./routes/planets/planets.router');
+
+// middleware
 app.use(express.json());
+
+// routes
+app.use('/api/v1/planets', planetsRouter);
 
 module.exports = app;
---
+++ server/src/routes/planets/planets.controller.js
@@ -0,0 +1,11 @@
+const { StatusCodes } = require('http-status-codes');
+
+const planets = require('../../models/planets.model');
+
+function getAllPlanets(req, res) {
+    return res.status(StatusCodes.OK).json(planets);
+};
+
+module.exports = {
+    getAllPlanets
+};
