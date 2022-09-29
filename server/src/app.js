7a79963bebbe3bc771b448ee6fd02e9564ccaade
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-29 22:07:54 +0300

    Add morgan logging middleware.

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+
 const app = express();
 
 const planetsRouter = require('./routes/planets/planets.router');
@@ -9,6 +11,9 @@ const planetsRouter = require('./routes/planets/planets.router');
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
