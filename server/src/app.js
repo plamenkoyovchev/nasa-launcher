ca9c8377a06aecd2013f0429a750f6dcd332b2a6
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-29 20:18:08 +0300

    Add static files middleware.

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -9,8 +10,12 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // routes
 app.use('/api/v1/planets', planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
