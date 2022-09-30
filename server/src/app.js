5dd1d94fe7f62ff422b33365dbd42db905a252b5
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-30 16:06:26 +0300

    Load index.html in any case when router doesn't match the required route.

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api/v1/planets', planetsRouter);
 app.use('/api/v1/launches', launchesRouter);
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
