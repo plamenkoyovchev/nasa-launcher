89cbefd32f9766512762312d39cd258613f7a29e
plamenkoyovchev <plamenkooo@gmail.com>
2022-10-15 15:21:51 +0300

    Upsert and get planets.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 
-const planets = [];
+const planets = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
     return planet['koi_disposition'] === 'CONFIRMED' &&
@@ -19,7 +19,7 @@ function loadPlanetsData() {
             }))
             .on('data', data => {
                 if (isHabitablePlanet(data)) {
-                    planets.push(data);
+                    savePlanet(data);
                 }
             })
             .on('error', (err) => {
@@ -32,8 +32,16 @@ function loadPlanetsData() {
     });
 }
 
-function getAllPlanets() {
-    return planets;
+async function getAllPlanets() {
+    return await planets.find({});
+}
+
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({ keplerName: planet.kepler_name }, { keplerName: planet.kepler_name }, { upsert: true });
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = {
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -2,8 +2,8 @@ const { StatusCodes } = require('http-status-codes');
 
 const { getAllPlanets } = require('../../models/planets.model');
 
-function httpGetAllPlanets(req, res) {
-    return res.status(StatusCodes.OK).json(getAllPlanets());
+async function httpGetAllPlanets(req, res) {
+    return res.status(StatusCodes.OK).json(await getAllPlanets());
 };
 
 module.exports = {
