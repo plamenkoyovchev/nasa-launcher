8f75f3c0906f413f50c32b69d6437e4fe16638d2
plamenkoyovchev <plamenkooo@gmail.com>
2022-10-16 19:20:44 +0300

    Refactor rename.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 
-const planets = require('./planets.mongo');
+const planetsDb = require('./planets.mongo');
 
 function isHabitablePlanet(planet) {
     return planet['koi_disposition'] === 'CONFIRMED' &&
@@ -33,12 +33,12 @@ function loadPlanetsData() {
 }
 
 async function getAllPlanets() {
-    return await planets.find({});
+    return await planetsDb.find({});
 }
 
 async function savePlanet(planet) {
     try {
-        await planets.updateOne({ keplerName: planet.kepler_name }, { keplerName: planet.kepler_name }, { upsert: true });
+        await planetsDb.updateOne({ keplerName: planet.kepler_name }, { keplerName: planet.kepler_name }, { upsert: true });
     } catch (error) {
         console.error(error);
     }
